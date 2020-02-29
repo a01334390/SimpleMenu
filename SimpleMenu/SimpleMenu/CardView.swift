9bb4d143c8e237ad5a73bfb3743d75523a598016
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-29 16:14:32 -0600

    Finished the Card View
    This view looks nice now

--- SimpleMenu/SimpleMenu/CardView.swift
+++ SimpleMenu/SimpleMenu/CardView.swift
@@ -10,13 +10,49 @@ import SwiftUI
 
 struct CardView: View {
     
-    // MARK - CARD
+    //MARK: - Properties
+    var gradient : [Color] = [Color("Color01"),Color("Color02")]
+    // MARK: - Card
     var body: some View {
         ZStack {
-            Text("CARD")
+            Image("developer-no1")
+            VStack {
+                Text("Swift UI")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                Text("Better apps. Less code")
+                    .fontWeight(.light)
+                    .foregroundColor(Color.white)
+                    .italic()
+            }.offset(y: -218)
+            Button(action: {
+                print("Button was tapped")
+            }) {
+                HStack {
+                    Text("Learn".uppercased())
+                        .fontWeight(.heavy)
+                        .accentColor(Color.white)
+                        .foregroundColor(Color.white)
+                    Image(systemName: "arrow.right.circle")
+                        .font(Font.title.weight(.medium))
+                        .accentColor(Color.white)
+                }
+                .padding(.vertical)
+                .padding(.horizontal, 24)
+                .background(LinearGradient(gradient: Gradient(colors: gradient),
+                                           startPoint: .leading,
+                                           endPoint: .trailing))
+                .clipShape(Capsule())
+                .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
+            }
+            .offset(y: 210)
         }
         .frame(width: 335, height: 545)
-        .background(Color.pink)
+        .background(LinearGradient(gradient: Gradient(colors: gradient),
+                                   startPoint: .top,
+                                   endPoint: .bottom))
         .cornerRadius(26)
         .shadow(radius: 8)
     }
